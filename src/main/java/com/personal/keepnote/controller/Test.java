9879babd8b0fa6704c9a3e5baec844794e815fb4
fengzhi <1336143962@qq.com>
2019-06-20 18:19:26 +0800

    引入swagger

--- src/main/java/com/personal/keepnote/controller/Test.java
+++ src/main/java/com/personal/keepnote/controller/Test.java
@@ -2,6 +2,7 @@ package com.personal.keepnote.controller;
 
 import com.personal.keepnote.persistence.dao.SysUserMapper;
 import com.personal.keepnote.persistence.entity.SysUserExample;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
     @Autowired
     SysUserMapper sysUserMapper;
+
     @RequestMapping("/")
-    public String test(){
+    public String test() {
         return "hello,word!";
     }
+
     @RequestMapping("/aa")
-    public String aa(){
+    public String aa() {
         SysUserExample sysUserExample = new SysUserExample();
         sysUserMapper.selectByExample(sysUserExample);
         return "hello,word!";
