40c828fb49a5d18d791a23ed5ef545f78dab2918
fengzhi <1336143962@qq.com>
2019-06-21 18:04:51 +0800

    添加新增项目接口

--- src/main/java/com/personal/keepnote/controller/UseTypeController.java
+++ src/main/java/com/personal/keepnote/controller/UseTypeController.java
@@ -1,14 +1,15 @@
 package com.personal.keepnote.controller;
 
+import com.personal.keepnote.common.entity.ResultEntity;
 import com.personal.keepnote.common.entity.ResultEntityGenerics;
 import com.personal.keepnote.persistence.entity.UseType;
 import com.personal.keepnote.service.UseTypeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -26,5 +27,22 @@ public class UseTypeController {
     public ResultEntityGenerics<List<UseType>> topicType() {
         List<UseType> allTopicUseType = useTypeService.getAllTopicUseType();
         return new ResultEntityGenerics().newResultEntity(allTopicUseType);
+    }
+
+    @ApiOperation(value = "查询子类型", notes = "查询子类型")
+    @RequestMapping(value = "/topic/type", method = RequestMethod.GET)
+    public ResultEntityGenerics<List<UseType>> childType(@ApiParam(name = "pid", value = "父id", required = true) @RequestParam(value = "pid") String pid) {
+        if (StringUtils.isEmpty(pid)) {
+            return new ResultEntityGenerics().newResultEntity(-1, "err", "数据类型有误");
+        }
+        List<UseType> allTopicUseType = useTypeService.getAllChildUseType(Integer.parseInt(pid));
+        return new ResultEntityGenerics().newResultEntity(allTopicUseType);
+    }
+
+    @ApiOperation(value = "查询子类型", notes = "查询子类型")
+    @RequestMapping(value = "/topic/type", method = RequestMethod.GET)
+    public ResultEntity topicType(@ApiParam(name = "useType", value = "类别对象", required = true) @RequestBody UseType useType) {
+        useTypeService.addUseType(useType);
+        return ResultEntity.newResultEntity("添加成功");
     }
 }
--- src/main/java/com/personal/keepnote/service/impl/UseTypeServiceImpl.java
+++ src/main/java/com/personal/keepnote/service/impl/UseTypeServiceImpl.java
@@ -1,8 +1,41 @@
 package com.personal.keepnote.service.impl;
 
-import com.personal.keepnote.service.InfoTypeService;
+
+import com.personal.keepnote.persistence.dao.UseTypeMapper;
+import com.personal.keepnote.persistence.entity.UseType;
+import com.personal.keepnote.persistence.entity.UseTypeExample;
+import com.personal.keepnote.service.UseTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class UseTypeServiceImpl implements InfoTypeService {
+public class UseTypeServiceImpl implements UseTypeService {
+    @Autowired
+    UseTypeMapper useTypeMapper;
+
+    @Override
+    public List<UseType> getAllTopicUseType() {
+        UseTypeExample useTypeExample = new UseTypeExample();
+        useTypeExample.createCriteria().andPidEqualTo(0);
+        return useTypeMapper.selectByExample(useTypeExample);
+    }
+
+    @Override
+    public List<UseType> getAllChildUseType(Integer pid) {
+        UseTypeExample useTypeExample = new UseTypeExample();
+        useTypeExample.createCriteria().andPidEqualTo(pid);
+        return useTypeMapper.selectByExample(useTypeExample);
+    }
+
+    @Override
+    public int addUseType(UseType useType) {
+        return useTypeMapper.insert(useType);
+    }
+
+    @Override
+    public int deleteUseType(Integer id) {
+        return useTypeMapper.deleteByPrimaryKey(id);
+    }
 }
