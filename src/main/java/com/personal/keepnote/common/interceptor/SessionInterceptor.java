8bb87d71891407084013cc19d45617e89046278e
fengzhi <1336143962@qq.com>
2019-06-24 16:43:24 +0800

    添加拦截器机制

--- src/main/java/com/personal/keepnote/common/interceptor/SessionInterceptor.java
+++ src/main/java/com/personal/keepnote/common/interceptor/SessionInterceptor.java
@@ -1,9 +1,39 @@
 package com.personal.keepnote.common.interceptor;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author MR FENG
  */
 public class SessionInterceptor extends HandlerInterceptorAdapter {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        Object user = httpServletRequest.getSession().getAttribute("user");
+        if (user!=null){
+            return true;
+        }else{
+
+
+
+
+            return false;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+
 }
